3a43235668bdff010d7d038fc09b24a25fe33f85
Dustin Lactin <dustin.lactin@gmail.com>
2025-11-19 22:04:56 -0700

    refactor: moved GetRepoRoot function into git package

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -3,12 +3,10 @@ package git
 import (
 	"os"
 	"testing"
-
-	"github.com/dlactin/rdv/internal/diff"
 )
 
 func TestSetupWorkTree(t *testing.T) {
-	repoRoot, _ := diff.GetRepoRoot()
+	repoRoot, _ := GetRepoRoot()
 
 	t.Run("Success with valid ref", func(t *testing.T) {
 		gitRef := "HEAD"
